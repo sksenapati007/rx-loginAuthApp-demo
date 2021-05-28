494af04d45ee6c476bc8f88fae8335c6f924f689
shubhendu <shubhendukumars09@gmail.com>
2021-05-29 03:59:32 +0530

    path changes

--- src/components/DashBoard.js
+++ src/components/DashBoard.js
@@ -1,7 +1,7 @@
 import React, { useContext } from "react";
 import { Redirect } from "react-router-dom";
 import { AuthContext } from "./Auth";
-import firebaseConfig from "../config.js";
+import firebaseConfig from "../../config.js";
 
 const DashBoard = () => {
     const { currentUser } = useContext(AuthContext);
--- src/components/SignUp.js
+++ src/components/SignUp.js
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import { Redirect } from "react-router-dom";
-import firebaseConfig from "../config";
+import firebaseConfig from "../../config.js";
 
 const SignUp = () => {
     const [currentUser, setCurrentUser] = useState(null);
